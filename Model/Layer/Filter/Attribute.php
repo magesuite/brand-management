1071712c1a974be18b4a2f2da0f91e3bed581e53
Grzegorz Bogusz <grzegorz.bogusz@creativestyle.pl>
2019-07-16 15:18:53 +0200

    Removed non-existing interface

--- Model/Layer/Filter/Attribute.php
+++ Model/Layer/Filter/Attribute.php
@@ -5,7 +5,7 @@ namespace MageSuite\BrandManagement\Model\Layer\Filter;
 use Smile\ElasticsuiteCore\Search\Request\BucketInterface;
 
 
-class Attribute extends \Smile\ElasticsuiteCatalog\Model\Layer\Filter\Attribute implements \Smile\ElasticsuiteCatalog\Model\Layer\Filter\FilterInterface
+class Attribute extends \Smile\ElasticsuiteCatalog\Model\Layer\Filter\Attribute
 {
 
 }
