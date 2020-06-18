84d13a94b7f3b4d6db2e2e0e5d3fa4dba0914c36
Paweł Knap <pawel.knap@creativestyle.pl>
2020-06-18 13:17:05 +0200

    - LP-496 - add comment for brand meta title plugin with description why it has been done this way

--- Plugin/Framework/View/Page/Config/Renderer/BrandMetaTitle.php
+++ Plugin/Framework/View/Page/Config/Renderer/BrandMetaTitle.php
@@ -1,6 +1,14 @@
 <?php
 namespace MageSuite\BrandManagement\Plugin\Framework\View\Page\Config\Renderer;
 
+/**
+ * This plugin changes meta title for brand page views, if meta title is set in admin.
+ * The reason for this is that Magento's \Magento\Framework\View\Page\Config::setMetaTitle method really changes
+ * <meta name="title" content="..." /> (which is ignored by Google Robot) instead of <title>.
+ * \Magento\Framework\View\Page\Config::getTitle()->set, cannot be used in Controller because this title is later used
+ * in page.main.title blocks to output H1, and \MageSuite\ThemeHelpers\Block\Category\View\Headline::getHeadline,
+ * and above two should not be affected by meta title set for brand in admin.
+ */
 class BrandMetaTitle
 {
     /**
