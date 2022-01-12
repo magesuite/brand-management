fbcad4c84cbd3059dbd110e503884eb416f2f851
Rafał Jękosz <rafal.jekosz@creativestyle.pl>
2022-01-12 12:33:25 +0000

    feat(pdp): [LP-1211] Add possibility to retrieve a icon url from viewModel

--- ViewModel/BrandRenderer.php
+++ ViewModel/BrandRenderer.php
@@ -8,9 +8,36 @@ class BrandRenderer implements \Magento\Framework\View\Element\Block\ArgumentInt
      */
     protected $configuration;
 
-    public function __construct(\MageSuite\BrandManagement\Helper\Configuration $configuration)
+    /**
+     * @var \MageSuite\BrandManagement\Api\BrandsRepositoryInterface
+     */
+    protected $brandsRepository;
+
+    public function __construct(
+        \MageSuite\BrandManagement\Helper\Configuration $configuration,
+        \MageSuite\BrandManagement\Api\BrandsRepositoryInterface $brandsRepository
+    )
     {
         $this->configuration = $configuration;
+        $this->brandsRepository = $brandsRepository;
+    }
+
+    public function getFirstBrand(\Magento\Catalog\Api\Data\ProductInterface $product): ?\MageSuite\BrandManagement\Api\Data\BrandsInterface
+    {
+        $brands = $product->getData('brand');
+
+        if (empty($brands)) {
+            return null;
+        }
+
+        $brandsIds = explode(',', $brands);
+        $brandId = reset($brandsIds);
+
+        if (empty($brandId)) {
+            return null;
+        }
+
+        return $this->brandsRepository->getById($brandId);
     }
 
     public function getBrandName($product, $location)
