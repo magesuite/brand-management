0a403dfdeb0606252bf5d9502f9a01f29d68094c
Krzysztof Tomczyk <krzysztof.tomczyk@creativestyle.pl>
2025-01-13 13:40:43 +0000

    Fix: [TOYS-1581] Save improved

--- Controller/Adminhtml/Brand/Save.php
+++ Controller/Adminhtml/Brand/Save.php
@@ -1,82 +1,57 @@
 <?php
 
+declare(strict_types=1);
+
 namespace MageSuite\BrandManagement\Controller\Adminhtml\Brand;
 
 class Save extends \Magento\Framework\App\Action\Action
 {
-    /**
-     * @var bool
-     */
-    protected $resultPage = false;
-    /**
-     * @var \Magento\Framework\View\Result\PageFactory
-     */
-    protected $pageFactory;
-    /**
-     * @var \MageSuite\BrandManagement\Model\Brands\Processor\SaveFactory
-     */
-    protected $saveFactory;
-    /**
-     * @var \MageSuite\BrandManagement\Validator\BrandParams
-     */
-    protected $brandParamsValidator;
-    /**
-     * @var \Magento\Framework\DataObjectFactory
-     */
-    protected $dataObjectFactory;
-
-    /**
-     * Save constructor.
-     * @param \Magento\Backend\App\Action\Context $context
-     * @param \Magento\Framework\View\Result\PageFactory $pageFactory
-     * @param \MageSuite\BrandManagement\Model\Brands\Processor\SaveFactory $saveFactory
-     * @param \MageSuite\BrandManagement\Validator\BrandParams $brandParamsValidator
-     */
     public function __construct(
         \Magento\Backend\App\Action\Context $context,
-        \Magento\Framework\View\Result\PageFactory $pageFactory,
-        \MageSuite\BrandManagement\Model\Brands\Processor\SaveFactory $saveFactory,
-        \MageSuite\BrandManagement\Validator\BrandParams $brandParamsValidator,
-        \Magento\Framework\DataObjectFactory $dataObjectFactory
-    )
-    {
-        $this->pageFactory = $pageFactory;
-        $this->saveFactory = $saveFactory;
-        $this->brandParamsValidator = $brandParamsValidator;
+        protected \Magento\Framework\View\Result\PageFactory $pageFactory,
+        protected \MageSuite\BrandManagement\Model\Brands\Processor\SaveFactory $saveFactory,
+        protected \MageSuite\BrandManagement\Validator\BrandParams $brandParamsValidator,
+        protected \Magento\Framework\DataObjectFactory $dataObjectFactory,
+    ) {
         parent::__construct($context);
-        $this->dataObjectFactory = $dataObjectFactory;
     }
 
-    /**
-     * @return \Magento\Framework\Controller\Result\Redirect
-     */
-    public function execute()
+    public function execute(): \Magento\Framework\Controller\Result\Redirect
     {
         $params = $this->_request->getParams();
+
         try {
             $params['is_api'] = false;
             $this->brandParamsValidator->validateParams($params);
 
             $paramsObject = $this->dataObjectFactory->create();
             $paramsObject->setData($params);
-            $this->saveFactory->create()->processSave($paramsObject);
+            $savedBrand = $this->saveFactory->create()->processSave($paramsObject);
             $this->messageManager->addSuccessMessage('Brand has been saved');
-        } catch (\Exception $e)
-        {
+            $params['id'] = $savedBrand->getId();
+
+            if (!empty($params['store_id'])) {
+                $params['store'] = $params['store_id'];
+            }
+
+            $url = $this->_url->getUrl('brands/brand/edit', $params);
+        } catch (\Exception $e) {
             $this->messageManager->addErrorMessage($e->getMessage());
+
+            if (!empty($params['entity_id'])) {
+                $url = $this->_url->getUrl('brands/brand/edit', ['id' => $params['entity_id']]);
+            } else {
+                $url = $this->_url->getUrl('brands/brand/newbrand');
+            }
         }
+
         $resultRedirect = $this->resultRedirectFactory->create();
-        $storeId = isset($params['store_id']) ? $params['store_id'] : 0;
-        $url = $this->_url->getUrl('brands/brand/edit', ['id' => $params['entity_id'], 'store' => $storeId]);
         $resultRedirect->setPath($url);
 
         return $resultRedirect;
     }
 
-    /**
-     * @return bool
-     */
-    protected function _isAllowed()
+    protected function _isAllowed(): bool
     {
         return true;
     }
