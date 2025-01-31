1c7e6786d104afff9d515515290c0dc366577292
Oleksandr Ivasiunko <olivas@smile.fr>
2025-01-31 10:31:57 +0000

    feat: [TOYS-1587] added possibility to hide brand page title

---
+++ Setup/Patch/Data/AddBrandHideTitleAttribute.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MageSuite\BrandManagement\Setup\Patch\Data;
+
+class AddBrandHideTitleAttribute implements \Magento\Framework\Setup\Patch\DataPatchInterface
+{
+    protected \Magento\Framework\Setup\ModuleDataSetupInterface $moduleDataSetup;
+    protected \Magento\Eav\Setup\EavSetupFactory $eavSetupFactory;
+
+    public function __construct(
+        \Magento\Framework\Setup\ModuleDataSetupInterface $moduleDataSetup,
+        \Magento\Eav\Setup\EavSetupFactory $eavSetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    public function apply(): void
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $eavSetup->addAttribute(
+            \MageSuite\BrandManagement\Model\Brands::ENTITY,
+            \MageSuite\BrandManagement\Model\Brand::BRAND_HIDE_HEADLINE_ATTRIBUTE_CODE,
+            [
+                'type' => 'int',
+                'label' => 'Hide Headline',
+                'input' => 'boolean',
+                'required' => false,
+                'sort_order' => 90,
+                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
+                'default' => 0,
+                'source' => \Magento\Eav\Model\Entity\Attribute\Source\Boolean::class,
+            ]
+        );
+        $this->moduleDataSetup->endSetup();
+    }
+
+    public function getAliases(): array
+    {
+        return [];
+    }
+
+    public static function getDependencies(): array
+    {
+        return [];
+    }
+}
+
